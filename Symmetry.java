effbb6d0c0eb64bc679df6bc7089dd93b677635c
PatrikZhong <zhong.patrik@gmail.com>
2020-07-07 21:40:47 +0200

    commiting before trying my swap method

--- Symmetry.java
+++ Symmetry.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 public class Symmetry {
-    
-    public static void main (string[] args){
+    public static void main (String[] args){
         
+        String[] printArray = new String[l000000];
+        Scanner in = new Scanner(System.in);
+
+        int lengths = in.nextInt();
+
+        while(lengths!= 0){
+
+            String[] nameArr = new String[lengths];
+
+                for(int i = 0; i < lengths; i++){
+                    String name = in.next();
+                    nameArr[i] = name;
+                }
+
+            System.out.println(Arrays.toString(nameArr)); //här har vi en del-array med namn i sig
+          
+            for(int i = 0; i < lengths; i++){
+                
+
+            }
+        }               
     }
 }
